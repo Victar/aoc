e7b08336ad0f6f66f16378ae0107092e9cc71ad3
Viktar Kadol <viktar.kadol@kaufland.com>
2021-12-15 09:49:15 +0100

    added few more from previous year

---
+++ src/test/java/adventofcode/year2019/Day2.java
@@ -0,0 +1,67 @@
+package adventofcode.year2019;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import adventofcode.BaseTest;
+
+public class Day2 extends BaseTest {
+
+	@Test public void runSilver() throws Exception {
+		final ArrayList<String> data = readStringFromFile("year2019/day2/input.txt");
+		final List<Integer> result = new ArrayList<Integer>();
+		final String[] arr = data.get(0).split(",");
+		for (int i = 0; i < arr.length; i++) {
+			result.add(Integer.parseInt(arr[i]));
+		}
+		result.set(1, 12);
+		result.set(2, 2);
+		boolean process = true;
+		for (int i = 0; i < result.size() && process; i = i + 4) {
+			final int current = result.get(i);
+			if (current == 99) {
+				process = false;
+			} else if (current == 1) {
+				result.set(result.get(i + 3), result.get(result.get(i + 1)) + result.get(result.get(i + 2)));
+			} else if (current == 2) {
+				result.set(result.get(i + 3), result.get(result.get(i + 1)) * result.get(result.get(i + 2)));
+			}
+		}
+		System.out.println(result.get(0));
+	}
+
+	@Test public void runGold() throws Exception {
+		final ArrayList<String> data = readStringFromFile("year2019/day2/input.txt");
+		final List<Integer> init = new ArrayList<Integer>();
+		final String[] arr = data.get(0).split(",");
+		for (int i = 0; i < arr.length; i++) {
+			init.add(Integer.parseInt(arr[i]));
+		}
+
+		boolean found = false;
+		for (int n = 0; n < 100 && !found; n++) {
+			for (int v = 0; v < 100 && !found; v++) {
+				final List<Integer> result = new ArrayList<>(init);
+				result.set(1, n);
+				result.set(2, v);
+				boolean process = true;
+				for (int i = 0; i < result.size() && process; i = i + 4) {
+					final int current = result.get(i);
+					if (current == 99) {
+						process = false;
+					} else if (current == 1) {
+						result.set(result.get(i + 3), result.get(result.get(i + 1)) + result.get(result.get(i + 2)));
+					} else if (current == 2) {
+						result.set(result.get(i + 3), result.get(result.get(i + 1)) * result.get(result.get(i + 2)));
+					}
+				}
+				if (result.get(0) == 19690720) {
+					System.out.println(n * 100 + v);
+					found = true;
+				}
+			}
+		}
+	}
+}
