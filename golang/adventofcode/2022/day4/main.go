11b7d1962ddd24745ea3c9c2b6b54cc7ffc53256
Viktar Kadol <viktar.kadol@mail.schwarz>
2022-12-04 06:29:29 +0100

    AOC golang 2022-4

---
+++ golang/adventofcode/2022/day4/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"adventofcode/util"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	runSilver()
+	runGold()
+}
+
+func runSilver() {
+	lines, err := util.ReadFile("year2022/day4/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	var score = 0
+	for _, line := range lines {
+		r := regexp.MustCompile("[^,-]+")
+		strarr := r.FindAllString(line, -1)
+		var s1b, _ = strconv.Atoi(strarr[0])
+		var s1e, _ = strconv.Atoi(strarr[1])
+		var s2b, _ = strconv.Atoi(strarr[2])
+		var s2e, _ = strconv.Atoi(strarr[3])
+
+		if s1b <= s2b && s1e >= s2e || s2b <= s1b && s2e >= s1e {
+			score++
+		}
+	}
+	println(score)
+
+}
+
+func runGold() {
+	lines, err := util.ReadFile("year2022/day4/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	var score = 0
+	for _, line := range lines {
+		r := regexp.MustCompile("[^,-]+")
+		strarr := r.FindAllString(line, -1)
+		var s1b, _ = strconv.Atoi(strarr[0])
+		var s1e, _ = strconv.Atoi(strarr[1])
+		var s2b, _ = strconv.Atoi(strarr[2])
+		var s2e, _ = strconv.Atoi(strarr[3])
+
+		if s1b <= s2b && s2b <= s1e || s2b <= s1b && s1b <= s2e {
+			score++
+		}
+	}
+	println(score)
+}
