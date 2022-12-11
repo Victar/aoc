982f6720819dc4b3e46a019203c9bb0a4d2fe042
Viktar Kadol <viktar.kadol@mail.schwarz>
2022-12-11 07:11:19 +0100

    AOC java 2022-11 code formatted

--- java/src/test/java/adventofcode/year2022/Day11.java
+++ java/src/test/java/adventofcode/year2022/Day11.java
@@ -10,14 +10,60 @@ import java.util.*;
 public class Day11 extends BaseTest {
 
 	public static final int DAY = 11;
+	Long commonDivider = 1l;
 
 	@Ignore @Test public void runDownloadInput() throws Exception {
 		downloadInput(DAY);
 	}
 
-	Long commonDivider=1l;
-	@Data
-	class Monkey {
+	@Test public void runGold() throws Exception {
+		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
+		data.add("");
+		Map<Integer, Monkey> monkeys = new HashMap<>();
+		for (int i = 0; i < data.size(); i = i + 7) {
+			Monkey current = new Monkey(data.get(i), data.get(i + 1), data.get(i + 2), data.get(i + 3), data.get(i + 4), data.get(i + 5));
+			monkeys.put(current.id, current);
+			commonDivider = commonDivider * current.getTestNumber();
+		}
+
+		for (int j = 0; j < 10000; j++) {
+			for (int i = 0; i < monkeys.size(); i++) {
+				monkeys.get(i).processRound(monkeys, true);
+			}
+		}
+		List<Long> inspections = new ArrayList<>();
+		for (Map.Entry<Integer, Monkey> entry : monkeys.entrySet()) {
+			inspections.add(entry.getValue().inspectionCount);
+		}
+		Collections.sort(inspections);
+		System.out.println(inspections.get(inspections.size() - 1) * inspections.get(inspections.size() - 2));
+
+	}
+
+	@Test public void runSilver() throws Exception {
+		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
+		data.add("");
+		Map<Integer, Monkey> monkeys = new HashMap<>();
+		for (int i = 0; i < data.size(); i = i + 7) {
+			Monkey current = new Monkey(data.get(i), data.get(i + 1), data.get(i + 2), data.get(i + 3), data.get(i + 4), data.get(i + 5));
+			monkeys.put(current.id, current);
+		}
+
+		for (int j = 0; j < 20; j++) {
+			for (int i = 0; i < monkeys.size(); i++) {
+				monkeys.get(i).processRound(monkeys, false);
+			}
+		}
+		List<Long> inspections = new ArrayList<>();
+		for (Map.Entry<Integer, Monkey> entry : monkeys.entrySet()) {
+			inspections.add(entry.getValue().inspectionCount);
+		}
+		Collections.sort(inspections);
+		System.out.println(inspections.get(inspections.size() - 1) * inspections.get(inspections.size() - 2));
+	}
+
+	@Data class Monkey {
+
 		int id;
 		List<Long> currentItems = new ArrayList<>();
 
@@ -27,30 +73,29 @@ public class Day11 extends BaseTest {
 		int testFalseMonkeyId;
 		long inspectionCount = 0;
 
-
-		Monkey(String nName, String sStart, String sOperation, String sTest, String sTestTrue, String sTestFalse){
-			this.id = Integer.parseInt(""+nName.charAt(7));
+		Monkey(String nName, String sStart, String sOperation, String sTest, String sTestTrue, String sTestFalse) {
+			this.id = Integer.parseInt("" + nName.charAt(7));
 			String[] strS = sStart.substring(18).split(", ");
-			for (String s: strS){
+			for (String s : strS) {
 				currentItems.add(Long.parseLong(s));
 			}
 			this.operation = sOperation.substring(17);
-			this.testNumber = Integer.parseInt( sTest.substring(21));
-			this.testTrueMonkeyId = Integer.parseInt( sTestTrue.substring(29));
-			this.testFalseMonkeyId = Integer.parseInt( sTestFalse.substring(30));
+			this.testNumber = Integer.parseInt(sTest.substring(21));
+			this.testTrueMonkeyId = Integer.parseInt(sTestTrue.substring(29));
+			this.testFalseMonkeyId = Integer.parseInt(sTestFalse.substring(30));
 		}
 
-		public void processRound(Map<Integer, Monkey> monkeys, boolean gold){
-			for (Long current : currentItems){
+		public void processRound(Map<Integer, Monkey> monkeys, boolean gold) {
+			for (Long current : currentItems) {
 				Long currWorry = getNewWorry(current);
-				if (gold){
+				if (gold) {
 					currWorry = currWorry % commonDivider;
-				}else{
+				} else {
 					currWorry = currWorry / 3;
 				}
-				if (currWorry%testNumber == 0){
+				if (currWorry % testNumber == 0) {
 					monkeys.get(testTrueMonkeyId).currentItems.add(currWorry);
-				}else{
+				} else {
 					monkeys.get(testFalseMonkeyId).currentItems.add(currWorry);
 				}
 				this.inspectionCount++;
@@ -58,58 +103,11 @@ public class Day11 extends BaseTest {
 			this.currentItems.clear();
 		}
 
-
 		public Long getNewWorry(Long current) {
-			String last = operation.split(" ")[operation.split(" ").length-1];
+			String last = operation.split(" ")[operation.split(" ").length - 1];
 			Long number = last.equals("old") ? current : Long.parseLong(last);
-			return operation.contains("+") ?  current + number : current*number;
+			return operation.contains("+") ? current + number : current * number;
 		}
-	}
-
-	@Test public void runSilver() throws Exception {
-		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
-		data.add("");
-		Map<Integer, Monkey> monkeys = new HashMap<>();
-		for (int i=0; i<data.size(); i=i+7){
-			Monkey current = new Monkey(data.get(i), data.get(i+1), data.get(i+2), data.get(i+3), data.get(i+4), data.get(i+5));
-			monkeys.put(current.id, current);
-			commonDivider = commonDivider*current.getTestNumber();
-		}
-
-		for (int j=0; j<10000; j++){
-			for (int i=0; i< monkeys.size(); i++){
-				monkeys.get(i).processRound(monkeys, true);
-			}
-		}
-		List<Long> inspections = new ArrayList<>();
-		for (Map.Entry<Integer, Monkey> entry : monkeys.entrySet()) {
-			inspections.add(entry.getValue().inspectionCount);
-		}
-		Collections.sort(inspections);
-		System.out.println(inspections.get(inspections.size()-1) *inspections.get(inspections.size()-2));
-
-	}
-
-	@Test public void runGold() throws Exception {
-		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
-		data.add("");
-		Map<Integer, Monkey> monkeys = new HashMap<>();
-		for (int i=0; i<data.size(); i=i+7){
-			Monkey current = new Monkey(data.get(i), data.get(i+1), data.get(i+2), data.get(i+3), data.get(i+4), data.get(i+5));
-			monkeys.put(current.id, current);
-		}
-
-		for (int j=0; j<20; j++){
-			for (int i=0; i< monkeys.size(); i++){
-				monkeys.get(i).processRound(monkeys, false);
-			}
-		}
-		List<Long> inspections = new ArrayList<>();
-		for (Map.Entry<Integer, Monkey> entry : monkeys.entrySet()) {
-			inspections.add(entry.getValue().inspectionCount);
-		}
-		Collections.sort(inspections);
-		System.out.println(inspections.get(inspections.size()-1) *inspections.get(inspections.size()-2));
 	}
 
 }
