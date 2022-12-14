601da3096f0516d88ddd5cd086f30f93a13543f4
Viktar Kadol <viktar.kadol@mail.schwarz>
2022-12-14 10:13:46 +0100

    AOC java 2022-14 fixed bug, optimised time from 4min to 3 sec for 2nd part, added drawing

--- java/src/test/java/adventofcode/year2022/Day14.java
+++ java/src/test/java/adventofcode/year2022/Day14.java
@@ -5,8 +5,7 @@ import lombok.Data;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Day14 extends BaseTest {
 
@@ -18,20 +17,18 @@ public class Day14 extends BaseTest {
 		downloadInput(DAY);
 	}
 
-
 	@Test public void runSilver() throws Exception {
-		runAny( false);
-
+		runAny(false);
 	}
 
-
 	@Test public void runGold() throws Exception {
-		runAny( true);
+		runAny(true);
 	}
 
 	public void runAny(boolean isGold) throws Exception {
 		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
 		List<Point> listPoint = new ArrayList<>();
+		Set<String> setPointHashes = new HashSet<>();
 		MAX_Y = 0;
 		for (final String input : data) {
 			String[] borders = input.split(" -> ");
@@ -39,55 +36,92 @@ public class Day14 extends BaseTest {
 				listPoint.addAll(Point.getPoints(borders[i - 1], borders[i]));
 			}
 		}
-		if (isGold){
+		if (isGold) {
 			listPoint.addAll(Point.getPoints("-1000," + (MAX_Y + 1), "1500," + (MAX_Y + 1)));
 		}
 		int before = listPoint.size();
+		for (Point point : listPoint) {
+			setPointHashes.add(point.getHash());
+		}
 		boolean canAdd = true;
-		int count = 0;
+		final Point START = Point.of(500, 0);
 		while (canAdd) {
-			count++;
-			if (count>100000){ // Todo fix bug with enfless loop
-				break;
-			}
-			Point start = Point.of(500, 0);
+			Point start = START;
 			while (start != null) {
 				if (start.y >= MAX_Y) {
 					canAdd = false;
 					break;
 				}
-				start = addPoint(listPoint, start);
+				start = addPoint(listPoint, setPointHashes, start);
+			}
+			if (listPoint.contains(START)) {
+				canAdd = false;
 			}
 		}
-		System.out.println("size: " + (listPoint.size() - before));
+
+		System.out.println((listPoint.size() - before));
+		drawPoints(listPoint, 480, 520, 0, 15);
 	}
 
-	public Point addPoint(List<Point> listPoint, Point current) {
-		if (!listPoint.contains(Point.of(current.x, current.y + 1))) {
+	public Point addPoint(List<Point> listPoint, Set<String> setPointHashes, Point current) {
+		if (!setPointHashes.contains(current.x + "," + (current.y + 1))) {
 			return Point.of(current.x, current.y + 1);
 		}
-		if (!listPoint.contains(Point.of(current.x - 1, current.y + 1))) {
+		if (!setPointHashes.contains((current.x - 1) + "," + (current.y + 1))) {
 			return Point.of(current.x - 1, current.y + 1);
 		}
-		if (!listPoint.contains(Point.of(current.x + 1, current.y + 1))) {
+		if (!setPointHashes.contains((current.x + 1) + "," + (current.y + 1))) {
 			return Point.of(current.x + 1, current.y + 1);
 		}
 		if (!listPoint.contains(current)) {
 			listPoint.add(current);
+			setPointHashes.add(current.getHash());
 			return null;
 		}
 		return null;
+	}
+
+	public void drawPoints(List<Point> listPoint, int startX, int endX, int startY, int endY) {
+		System.out.println("=Draw= (" + startX + "->" + endX + ")  (" + startY + "->" + endY + ")");
+		Map<String, Point> map = new HashMap<>();
+		for (Point point : listPoint) {
+			if (!map.containsKey(point.getHash())) {
+				map.put(point.getHash(), point);
+			}
+		}
+		for (int y = startY; y < endY; y++) {
+			StringBuilder sb = new StringBuilder();
+			for (int x = startX; x < endX; x++) {
+				String cur = " ";
+				Point p = map.get(x + "," + y);
+				if (p != null) {
+					cur = p.isWall ? "#" : "0";
+				}
+				sb.append(cur);
+			}
+			System.out.println(sb);
+		}
+		System.out.println("=End=\n\n");
 	}
 
 	@Data static class Point {
 
 		int x;
 		int y;
+		boolean isWall = false;
 
 		public static Point of(int x, int y) {
 			Point p = new Point();
 			p.x = x;
 			p.y = y;
+			return p;
+		}
+
+		public static Point ofWall(int x, int y) {
+			Point p = new Point();
+			p.x = x;
+			p.y = y;
+			p.isWall = true;
 			return p;
 		}
 
@@ -102,10 +136,28 @@ public class Day14 extends BaseTest {
 			for (int x = sX; x <= eX; x++) {
 				for (int y = sY; y <= eY; y++) {
 					MAX_Y = Math.max(MAX_Y, y + 1);
-					wall.add(Point.of(x, y));
+					Point p = Point.of(x, y);
+					p.setWall(true);
+					wall.add(p);
+
 				}
 			}
 			return wall;
+		}
+
+		public String getHash() {
+			return x + "," + y;
+		}
+
+		@Override public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Point point = (Point) o;
+			return x == point.x && y == point.y;
+		}
+
+		@Override public int hashCode() {
+			return Objects.hash(x, y);
 		}
 	}
 
