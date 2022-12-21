8d7366f3e639eaf2c477cbe76472f2ccf4206de6
Viktar Kadol <viktar.kadol@mail.schwarz>
2022-12-21 10:06:57 +0100

    AOC java 2022-21 added auto-solver for part 2

--- java/src/test/java/adventofcode/year2022/Day21.java
+++ java/src/test/java/adventofcode/year2022/Day21.java
@@ -19,7 +19,7 @@ public class Day21 extends BaseTest {
 
 	@Test public void runSilver() throws Exception {
 		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
-		Map<String,Monkey> monkeys= new HashMap<>();
+		Map<String, Monkey> monkeys = new HashMap<>();
 		Monkey root = null;
 		for (final String input : data) {
 			Monkey current = new Monkey(input);
@@ -29,56 +29,54 @@ public class Day21 extends BaseTest {
 	}
 
 	@Test public void runGold() throws Exception {
-		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input.txt");
-		Map<String,Monkey> monkeys= new HashMap<>();
-		Monkey root = null;
-		Monkey rootLeft = null;
-		Monkey rootRight = null;
-		String meName = "humn";
+		final ArrayList<String> data = readStringFromFile("year2022/day" + DAY + "/input2.txt");
+		Map<String, Monkey> monkeys = new HashMap<>();
 		for (final String input : data) {
 			Monkey current = new Monkey(input);
 			monkeys.put(current.name, current);
 		}
-		HashMap<String, Long> DP = new HashMap<>();
-		root = monkeys.get("root");
-		rootLeft = monkeys.get(root.monkeyLeft);
-		rootRight = monkeys.get(root.monkeyRight);
-		long i  = 3352886100000L;
-//		long i  = 52716091087786
-//		52716091090000 leftNumber -730962523154826  52716091087786 summ -783678614242612
-// 		62716091090000 leftNumber -889720167626738  52716091087786 summ -942436258714524
-//		62716091110000 leftNumber -889720167944250  52716091087786 summ -942436259032036
-//		42716091090000 leftNumber -572204878682870  52716091087786 summ -624920969770656
-//		2990000005787 leftNumber 58477185777786  52716091087786    diff    5761094690000
-//		3000000011175 leftNumber 58318428047786  52716091087786    diff    5602336960000
-//		3200000008691 leftNumber 55143275197786  52716091087786    diff    2427184110000
-//		3500000002761 leftNumber 50380545957786  52716091087786    diff   -2335545130000
-//		3354999906694 leftNumber 52682533327786  52716091087786 diff -33557760000
-//		3354999909211 leftNumber 52682533287786  52716091087786 diff -33557800000
-//		3353999902563 leftNumber 52698409157786  52716091087786 diff -17681930000
-//		3352999901583 leftNumber 52714284937786  52716091087786 diff  -1806150000
-//		3356000009083 leftNumber 52666655937786  52716091087786 diff -49435150000
-//		3352500000615 leftNumber 52722221247786  52716091087786 diff   6130160000
-		while(true){
-			i++;
-			DP.clear();
-			DP.put(meName, i);
-			long leftNumber = rootLeft.getNumber(monkeys, DP);
-			long rightNumber = rootRight.getNumber(monkeys, DP);
-			if ((leftNumber - rightNumber)%10000==0){
-				System.out.println(i + " leftNumber " + leftNumber + "  " + rightNumber + " diff " + (leftNumber - rightNumber));
+		boolean found = false;
+		long start = 1L;
+		long end = 2L;
+		long offset = 0;
+		while (!found) {
+			long diffStart = getDiff(start + offset, monkeys);
+			long diffEnd = getDiff(end + offset, monkeys);
+			if (diffStart == 0 || diffEnd == 0) {
+				System.out.println("Found following answers:");
+				for (long j = offset - start; j <= offset + end; j++) {
+					if (getDiff(j, monkeys) == 0) {
+						System.out.println(j);
+					}
+				}
+				found = true;
 			}
-
-			if (leftNumber == rightNumber){
-				System.out.println(i);
-				break;
+			if (diffStart > 0 && diffEnd < 0 || diffStart < 0 && diffEnd > 0) {
+				offset = offset + start;
+				start = 1L;
+				end = 2L;
+			} else {
+				start = start * 2;
+				end = end * 2;
 			}
 		}
-//		System.out.println(monkeys.get("root").getNumber(monkeys, new HashMap<>()));
 	}
 
-	@Data
-	class Monkey{
+	public long getDiff(long i, Map<String, Monkey> monkeys) {
+		HashMap<String, Long> DP = new HashMap<>();
+		String meName = "humn";
+		Monkey root = monkeys.get("root");
+		Monkey rootLeft = monkeys.get(root.monkeyLeft);
+		Monkey rootRight = monkeys.get(root.monkeyRight);
+		DP.put(meName, i);
+		long leftNumber = rootLeft.getNumber(monkeys, DP);
+		long rightNumber = rootRight.getNumber(monkeys, DP);
+		//		System.out.println(i + " leftNumber:" + leftNumber + "  rightNumber:" + rightNumber + " diff " + (leftNumber - rightNumber));
+		return leftNumber - rightNumber;
+	}
+
+	@Data class Monkey {
+
 		String name;
 		String operation;
 		boolean isSimple;
@@ -87,16 +85,16 @@ public class Day21 extends BaseTest {
 		String monkeyRight;
 		String monkeyOperation;
 
-
-		public Monkey(String input){
+		public Monkey(String input) {
 			String[] arr = input.split(": ");
 			this.name = arr[0];
 			this.operation = arr[1];
-			try{
+			try {
 				this.simpleNumber = Integer.parseInt(arr[1]);
 				this.isSimple = true;
-			}catch(Exception ex){}
-			if (!isSimple){
+			} catch (Exception ignore) {
+			}
+			if (!isSimple) {
 				String[] ops = this.operation.split(" ");
 				monkeyLeft = ops[0];
 				monkeyOperation = ops[1];
@@ -104,27 +102,27 @@ public class Day21 extends BaseTest {
 			}
 		}
 
-		public long getNumber(Map<String, Monkey> monkeys, Map<String, Long> DP){
-			if (DP.containsKey(this.name)){
+		public long getNumber(Map<String, Monkey> monkeys, Map<String, Long> DP) {
+			if (DP.containsKey(this.name)) {
 				return DP.get(this.name);
 			}
-			if (isSimple){
+			if (isSimple) {
 				DP.put(this.name, this.simpleNumber);
 				return simpleNumber;
 			}
 			long leftNumber = monkeys.get(monkeyLeft).getNumber(monkeys, DP);
 			long rightNumber = monkeys.get(monkeyRight).getNumber(monkeys, DP);
 			long result = 0;
-			if ("+".equalsIgnoreCase(monkeyOperation)){
+			if ("+".equalsIgnoreCase(monkeyOperation)) {
 				return leftNumber + rightNumber;
 			}
-			if ("-".equalsIgnoreCase(monkeyOperation)){
+			if ("-".equalsIgnoreCase(monkeyOperation)) {
 				return leftNumber - rightNumber;
 			}
-			if ("*".equalsIgnoreCase(monkeyOperation)){
+			if ("*".equalsIgnoreCase(monkeyOperation)) {
 				return leftNumber * rightNumber;
 			}
-			if ("/".equalsIgnoreCase(monkeyOperation)){
+			if ("/".equalsIgnoreCase(monkeyOperation)) {
 				return leftNumber / rightNumber;
 			}
 
